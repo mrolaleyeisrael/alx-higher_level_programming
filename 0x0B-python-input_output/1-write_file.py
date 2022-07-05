f5509b462d52957fdc99bb646d8065027a1534b9
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-07-05 20:47:25 +0100

    writing a string into a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+A script that writes to a file and returns the number of characters
+"""
+
+def write_file(filename="", text=""):
+    """Function that returns the number of character written in a file"""
+    count = 0
+    with open(filename, mode='w' encoding='UTF+8') as filen:
+        for c in text:
+            filen.write(c)
+            count += 1
+    return (count)
