8d39d1273f2c56af2ba41cd55daa1e2c9027f4af
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-07-05 21:08:51 +0100

    string to JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Won so wipe this is a JSON script
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """A function like that"""
+    new = json.dump(my_obj)
+    return (new)
